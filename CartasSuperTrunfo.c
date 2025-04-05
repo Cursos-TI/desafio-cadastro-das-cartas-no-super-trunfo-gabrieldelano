ac0e042e1f28b4acba793213db4e169dc6b3e0e5
gabriel <gabrieldelano001@gmail.com>
2025-04-05 18:33:32 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,88 +2,97 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-
-int main() {
 #include <stdio.h>
 #include <locale.h>
-/*não consigo fazer a saída ter pontuação (ja tentei setlocale) não sei se é erro do meu compilardor,mas para fins de comodidade
-digite as cartas sem acentuação que irá dar o resultado.*/
-//aqui são as cartas que vão ser usadas
-//Carta 1
-setlocale(LC_ALL, "Portuguese_Brazil"); //aparecer acentos
-    char estado1[90];
-    char carta_1[40];
-    char nome_da_cidade_1[90];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontos_turisticos1;
-    printf("Digite o Estado: ");
-    fgets(estado1,90,stdin);
-    printf("O Estado é: %s \n",estado1);
-    printf("Codigo da carta: ");
-    fgets(carta_1,40,stdin);
-    printf("O código da carta é: %s \n",carta_1);
-    printf("Digite a cidade: ");
-    fgets(nome_da_cidade_1,90,stdin);
-    printf("O nome da cidade é: %s \n",nome_da_cidade_1);
-    printf("Digite o numero de habitantes :");
-    scanf("%d", &populacao1 );
-    printf("O numero de habitantes é: %d\n",populacao1);
-    printf("Digite a área: ");
-    scanf("%f", &area1); 
-    printf("A área é: %f \n",area1);
-    printf("Digite o PIB: ");
-    scanf("%f", &pib1);
-    printf("O PIB é: %f \n",pib1);
-    printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &pontos_turisticos1);
-    printf("O numero de pontos turisticos é: %d \n",pontos_turisticos1);
-
-
- //Carta 2
- 
-    char estado2[90];
-    char carta_2[40];
-    char nome_da_cidade_2[90];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontos_turisticos2;
-    printf("Digite o Estado: ");
-    fgets(estado2 , 90 , stdin);
-    fgets(estado2 , 90 , stdin); //adcionei mais um fgets porque não estava aparecendo o dado de entrada
-    printf("O Estado é: %s \n",estado2);
-    printf("Codigo da carta: ");
-    fgets(carta_2,40,stdin);
-    printf("O código da carta é: %s \n",carta_2);
-    printf("Digite a cidade: ");
-    fgets(nome_da_cidade_2,90,stdin);
-    printf("O nome da cidade é: %s \n",nome_da_cidade_2);
-    printf("Digite o numero de habitantes :");
-    scanf("%d", &populacao2 );
-    printf("O numero de habitantes é: %d\n",populacao2);
-    printf("Digite a área: ");
-    scanf("%f", &area2); 
-    printf("A área é: %f \n",area2);
-    printf("Digite o PIB: ");
-    scanf("%f", &pib2);
-    printf("O PIB é: %f \n",pib2);
-    printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &pontos_turisticos2);
-    printf("O numero de pontos turisticos é: %d \n",pontos_turisticos2);
-
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+int main() {
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+     /*não consigo fazer a saída ter pontuação (ja tentei setlocale) não sei se é erro do meu compilardor,mas para fins de comodidade
+     digite as cartas sem acentuação que irá dar o resultado.*/
+     //aqui são as cartas que vão ser usadas
+     //Carta 1
+     setlocale(LC_ALL, "Portuguese_Brazil"); //aparecer acentos
+        char estado1[90];
+        char carta_1[40];
+        char nome_da_cidade_1[90];
+        int populacao1;
+        float area1;
+        float pib1;
+        int pontos_turisticos1;
+        float densidade_populacional1 = (float) populacao1 / area1;
+        float pib_per_capita1 = pib1 / (float)populacao1;
+        printf("Digite o Estado: ");
+        fgets(estado1,90,stdin);
+        printf("O Estado é: %s \n",estado1);
+        printf("Codigo da carta: ");
+        fgets(carta_1,40,stdin);
+        printf("O código da carta é: %s \n",carta_1);
+        printf("Digite a cidade: ");
+        fgets(nome_da_cidade_1,90,stdin);
+        printf("O nome da cidade é: %s \n",nome_da_cidade_1);
+        printf("Digite o numero de habitantes :");
+        scanf("%d", &populacao1 );
+        printf("O numero de habitantes é: %d\n",populacao1);
+        printf("Digite a área: ");
+        scanf("%f", &area1); 
+        printf("A área é: %f\n",area1);
+        printf("Digite o PIB: ");
+        scanf("%f", &pib1);
+        printf("O PIB é: %f \n",pib1);
+        printf("Digite o numero de pontos turisticos: ");
+        scanf("%d", &pontos_turisticos1);
+        printf("O numero de pontos turisticos é: %d \n",pontos_turisticos1);
+        printf("A densidade populacional é: %.2f\n",densidade_populacional1);
+        printf("O PIB per capita é:  %.2f\n",pib_per_capita1);
 
-    return 0;
+  //Carta 2
+     
+        char estado2[90];
+        char carta_2[40];
+        char nome_da_cidade_2[90];
+        int populacao2;
+        float area2;
+        float pib2;
+        int pontos_turisticos2;
+        float densidade_populacional2 = (float) populacao2 / area2;
+        float pib_per_capita2 = pib2 / (float)populacao2;
+        printf("Digite o Estado: ");
+        fgets(estado2 , 90 , stdin);
+        fgets(estado2 , 90 , stdin); //adcionei mais um fgets porque não estava aparecendo o dado de entrada
+        printf("O Estado é: %s \n",estado2);
+        printf("Codigo da carta: ");
+        fgets(carta_2,40,stdin);
+        printf("O código da carta é: %s \n",carta_2);
+        printf("Digite a cidade: ");
+        fgets(nome_da_cidade_2,90,stdin);
+        printf("O nome da cidade é: %s \n",nome_da_cidade_2);
+        printf("Digite o numero de habitantes :");
+        scanf("%d", &populacao2 );
+        printf("O numero de habitantes é: %d\n",populacao2);
+        printf("Digite a área: ");
+        scanf("%f", &area2); 
+        printf("A área é: %f \n",area2);
+        printf("Digite o PIB: ");
+        scanf("%f", &pib2);
+        printf("O PIB é: %f \n",pib2);
+        printf("Digite o numero de pontos turisticos: ");
+        scanf("%d", &pontos_turisticos2);
+        printf("O numero de pontos turisticos é: %d \n",pontos_turisticos2);
+        printf("A densidade populacional é: %.2f\n",densidade_populacional2);
+        printf("O PIB per capita é:  %.2f\n",pib_per_capita2);
+    
+    
+        // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+        // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+        
+        // Cadastro das Cartas:
+        // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+        // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+        
+        // Exibição dos Dados das Cartas:
+        // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+        // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    
+ return 0;
 }
+    
