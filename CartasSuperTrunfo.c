8682b37faf1c493d2ccc68a7669abf9192adcb10
gabrieldelano <gabrieldelano001@gmail.com>
2025-04-04 21:29:29 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,24 +6,77 @@
 
 
 int main() {
-    //aqui vai as letras dos 8 estados
-    char estados[A,B,C,D,E,F,G,H];
-    //aqui é o código da carta
-    char A01[];
-    char B01[];
-    //aqui são as cartas que vão ser usadas
-    //Carta 1
-    char nome_da_cidade1[50]("São Paulo");
-    int população1;
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+/*não consigo fazer a saída ter pontuação (ja tentei setlocale) não sei se é erro do meu compilardor,mas para fins de comodidade
+digite as cartas sem acentuação que irá dar o resultado.*/
+//aqui são as cartas que vão ser usadas
+//Carta 1
+setlocale(LC_ALL, "Portuguese_Brazil"); //aparecer acentos
+    char estado1[90];
+    char carta_1[40];
+    char nome_da_cidade_1[90];
+    int populacao1;
     float area1;
     float pib1;
     int pontos_turisticos1;
-    //Carta 2
-    char nome_da_cidade2[50]("Rio de Janeiro");
-    int população2;
+    printf("Digite o Estado: ");
+    fgets(estado1,90,stdin);
+    printf("O Estado é: %s \n",estado1);
+    printf("Codigo da carta: ");
+    fgets(carta_1,40,stdin);
+    printf("O código da carta é: %s \n",carta_1);
+    printf("Digite a cidade: ");
+    fgets(nome_da_cidade_1,90,stdin);
+    printf("O nome da cidade é: %s \n",nome_da_cidade_1);
+    printf("Digite o numero de habitantes :");
+    scanf("%d", &populacao1 );
+    printf("O numero de habitantes é: %d\n",populacao1);
+    printf("Digite a área: ");
+    scanf("%f", &area1); 
+    printf("A área é: %f \n",area1);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+    printf("O PIB é: %f \n",pib1);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos1);
+    printf("O numero de pontos turisticos é: %d \n",pontos_turisticos1);
+
+
+ //Carta 2
+ 
+    char estado2[90];
+    char carta_2[40];
+    char nome_da_cidade_2[90];
+    int populacao2;
     float area2;
     float pib2;
     int pontos_turisticos2;
+    printf("Digite o Estado: ");
+    fgets(estado2 , 90 , stdin);
+    fgets(estado2 , 90 , stdin); //adcionei mais um fgets porque não estava aparecendo o dado de entrada
+    printf("O Estado é: %s \n",estado2);
+    printf("Codigo da carta: ");
+    fgets(carta_2,40,stdin);
+    printf("O código da carta é: %s \n",carta_2);
+    printf("Digite a cidade: ");
+    fgets(nome_da_cidade_2,90,stdin);
+    printf("O nome da cidade é: %s \n",nome_da_cidade_2);
+    printf("Digite o numero de habitantes :");
+    scanf("%d", &populacao2 );
+    printf("O numero de habitantes é: %d\n",populacao2);
+    printf("Digite a área: ");
+    scanf("%f", &area2); 
+    printf("A área é: %f \n",area2);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+    printf("O PIB é: %f \n",pib2);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos2);
+    printf("O numero de pontos turisticos é: %d \n",pontos_turisticos2);
+
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
