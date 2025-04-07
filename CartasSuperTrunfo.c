a659042b3ec816a3b28e2b42dda517b60991cd7f
gabriel <gabrieldelano001@gmail.com>
2025-04-07 17:18:54 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,8 +17,10 @@ int main() {
         float area1;
         float pib1;
         int pontos_turisticos1;
-        float densidade_populacional1 = (float)populacao1 / area1;
-        float pib_per_capita1 = pib1 / (float)populacao1;
+        float densidade_populacional1;
+        densidade_populacional1 = populacao1 / area1;
+        float pib_per_capita1;
+        pib_per_capita1 = pib1 / populacao1;
         printf("Digite o Estado: ");
         fgets(estado1,90,stdin);
         printf("O Estado é: %s \n",estado1);
@@ -30,7 +32,7 @@ int main() {
         printf("O nome da cidade é: %s \n",nome_da_cidade_1);
         printf("Digite o numero de habitantes :");
         scanf("%d", &populacao1 );
-        printf("O numero de habitantes é: %d\n",populacao1);
+        printf("O numero de habitantes é: %.d\n",populacao1);
         printf("Digite a área: ");
         scanf("%f", &area1); 
         printf("A área é: %f\n",area1);
@@ -52,8 +54,10 @@ int main() {
         float area2;
         float pib2;
         int pontos_turisticos2;
-        float densidade_populacional2 = (float) populacao2 / area2;
-        float pib_per_capita2 = pib2 / (float)populacao2;
+        float densidade_populacional2;
+        densidade_populacional2 = populacao2 / area2;
+        float pib_per_capita2;
+        pib_per_capita2 = pib2 / populacao2;
         printf("Digite o Estado: ");
         fgets(estado2 , 90 , stdin);
         fgets(estado2 , 90 , stdin); //adcionei mais um fgets porque não estava aparecendo o dado de entrada
@@ -85,6 +89,12 @@ int main() {
         printf("Pontos Turisticos %d\n", pontos_turisticos1 > pontos_turisticos2);
         printf("Densidade populacional: %d\n", densidade_populacional1 > densidade_populacional2);
         printf("PIB per capita: %d\n", pib_per_capita1 > pib_per_capita2);
+        if(pontos_turisticos1 > pontos_turisticos2){
+          printf("A primeira carta tem mais pontos turíticos");
+        } else{
+          printf("A segunda carta tem mais pontos turíticos");
+        }
+
        /*Notas de desenvolvimento: na comparação entre densidade populacional e PIB per capita,
        a segunda carta vence pelo motivo de não conseguir desenvolver direito o sistema de divisão.
        Mesmo convertendo os valores para números flutuantes, a operação não é efetuada direito.
